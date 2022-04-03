1c4ea575f4c2edc0ec856868974eaf253f2ffbe5
Terence <35042213+reshinto@users.noreply.github.com>
2022-04-03 20:35:09 +0800

    Create delete.sh

---
+++ delete.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# chmod a+x delete.sh
+
+# digital ocean cloud environment
+# sudo docker stop christmas
+# sudo docker rm christmas
+# sudo docker rmi christmas
+
+# local environment
+docker stop christmas
+docker rm christmas
+docker rmi christmas
