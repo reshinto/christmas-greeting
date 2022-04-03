8e79cfc7892f51390aefde96e542918f95e5d80d
Terence <35042213+reshinto@users.noreply.github.com>
2022-04-03 20:33:11 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 # chmod a+x run.sh
 
+# digital ocean cloud environment
+# sudo docker stop christmas
+# sudo docker rm christmas
+# sudo docker rmi christmas
+# sudo docker build -t christmas .
+# sudo docker run -d -p 2512:80 --name christmas --restart unless-stopped christmas
+
+# local environment
 PUBLIC_URL=. yarn run build
 docker stop christmas
 docker rm christmas
